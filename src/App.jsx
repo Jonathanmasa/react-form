6a67a0e4d014d8d1b27b81b58a98d136a2c08978
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-18 16:38:19 +0100

    draft[form/input]

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// importo bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+function App() {
+  const [tasks, setTasks] = useState(['Fare la spesa']);
+  const [newTask, setNewTask] = useState('');
+  const addTask = e => {
+    e.preventDefault();
+    tasks.push(newTask);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+<>
+  <form onSubmit={addTask}>
+
+    <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Inserisci nuovo articolo"
+    />
+
+    <button type="submit">Aggiungi</button>
+
+  </form>
+
+    lista articoli
+      <ul>
+        {tasks.map((task, i) => (
+          <li key={i}>{task}</li>
+        ))}
+      </ul>
+</>
+)}
+
 
 export default App
